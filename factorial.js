7c9064e3c4a57bd9908514e98d17161226c6288e
engWafula <wafulaallan5@gmail.com>
2022-01-18 12:09:23 +0300

    new  stuffs

---
+++ factorial.js
@@ -0,0 +1,13 @@
+//Factorial of  a  number  with  loops
+
+const factorial=(num)=>{
+    var result =1
+    for(var i=1;i<=num;i++){
+     result*=i
+    }
+    return result
+}
+console.log(factorial(4))
+
+
+//Factorial  of  a number  using  recursion
--- recursion.js
+++ recursion.js
@@ -1,11 +1,25 @@
-var tracker=0;
-const call_me=()=>{
-    tracker++;
-    if(tracker===3){
-    return "no looping"//everything below the  return statement  wont execute
+// var tracker=0;
+// const call_me=()=>{
+//     tracker++;
     
-    }
+//     if(tracker===3){
+//     return "no looping"//everything below the  return statement  wont execute
    
- return call_me("anytime")
+//     }
+
+//    return  call_me("anytime")
+// }
+// console.log(call_me())
+
+
+const LoopNtimes=(n)=>{
+    console.log("the number is ",n)
+    if(n<=1){
+        return "completed"
+    }
+    else{
+        return LoopNtimes(n-1)
+    }
 }
-console.log(call_me())
+
+console.log(LoopNtimes(9))
