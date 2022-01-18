a706d8ba2ca853e8587180fbce61b1ce4a7bf360
engWafula <wafulaallan5@gmail.com>
2022-01-18 17:55:09 +0300

    more  improvements

--- RecursionExercise.js
+++ RecursionExercise.js
@@ -45,6 +45,50 @@ var recursiveCountDown = function(n) {
     }
   return value
   }
-console.log(exponent(2,2))
+//console.log(exponent(2,2))
 
 //recursive solution
+
+const Exponent=(base,expo)=>{
+    if(expo==1){
+        return base
+    }
+    else if(expo==0){
+        return 1
+    }
+   
+        return base*exponent(base,--expo)
+    
+    
+}
+//console.log(Exponent(2,0));
+
+
+const  recursiveMultiplier=(arr,num)=>{
+    if(arr.length===0){
+        return arr
+    }
+    var last=arr.pop()
+    recursiveMultiplier(arr,num)
+    arr.push(last*num)
+    return arr
+}
+console.log(recursiveMultiplier([1,2,3],2))
+
+//6. Write a function 'recursiveReverse' that takes an array and uses recursion to return its contents in reverse order
+
+var recursiveReverse = function(arr) {
+    var reversedArr = [];
+    var addItems = function(orderedArr) {
+      if (orderedArr.length > 0) {
+        reversedArr.push(orderedArr.pop());
+        addItems(orderedArr);
+      }
+      return;
+    }
+  
+    addItems(arr);
+  
+    return reversedArr;
+  }
+// console.log( recursiveReverse([1,2,3,4]))
---
+++ looping.js
@@ -0,0 +1,6 @@
+function logNumbers(start, end) {
+    for(var i = start; i <= end; i++) {
+      console.log(i);
+    }
+  }
+  logNumbers(1,10)
