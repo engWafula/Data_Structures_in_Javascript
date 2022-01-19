92d20d5b0da33addf76db95b006656a1bb699c24
engWafula <wafulaallan5@gmail.com>
2022-01-19 15:53:25 +0300

    sorting algorithms

--- TimeComplexity.js
+++ TimeComplexity.js
@@ -14,3 +14,32 @@ countChars("walk");//
 
 //Time complexity is  0(n)
 
+
+var countChars = function(str){
+
+    return str.length;
+
+};
+
+countChars("dance");
+
+countChars("walk");
+
+//Time complexity  in 0(1)
+
+
+//How much more work would it take to get the 
+//length of 1 million char string?
+
+
+
+
+var myList = ["hello", "hola"];//0(1)
+
+myList.push("bonjour");//0(1)
+
+myList.unshift();//0(n)
+
+
+//calculate the time complexity for the 
+//native methods above (separately)
